7eea4b522ab62e93e77943f4b0266189e8e7be61
nielxfb <adamludnl@gmail.com>
2025-02-09 17:43:28 +0700

    fix: indentation

--- Experiments/main.py
+++ Experiments/main.py
@@ -2,10 +2,10 @@ from data_loader import DataLoader, DataLoaderSettings
 from custom_dataset import CustomDataset, DatasetSettings
 
 loader_settings = DataLoaderSettings(
-  dataset_link='dair-ai/emotion',
-  keys=['train', 'validation', 'test'],
-  label_col='label',
-  text_col='text'
+    dataset_link='dair-ai/emotion',
+    keys=['train', 'validation', 'test'],
+    label_col='label',
+    text_col='text'
 )
 
 loader = DataLoader(loader_settings=loader_settings)
