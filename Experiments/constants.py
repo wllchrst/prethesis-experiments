582a41add2a3a0a7fda6bf9682d56b533229b47f
William Christian <william.christian001@binus.ac.id>
2025-02-10 09:59:09 +0700

    feat: constants for dataset links

--- Experiments/constants.py
+++ Experiments/constants.py
@@ -1,1 +1,9 @@
 DISTIL_BERT = "distilbert-base-uncased"
+
+DATASETS_LINKS: list[str] = [
+    'AdamCodd/emotion-balanced',
+    'dair-ai/emotion',
+    # 'philschmid/emotion',
+    'SetFit/emotion',
+    'mteb/emotion'
+]
