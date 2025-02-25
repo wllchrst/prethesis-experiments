db804cacc70fc7c403c9d5e00330fba78101e2d6
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-25 19:13:23 +0700

    feat: stop words from nltk

--- PythonScripts/eda.py
+++ PythonScripts/eda.py
@@ -1,33 +1,12 @@
 # Easy data augmentation techniques for text classification
 # Jason Wei and Kai Zou
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet, stopwords
 import random
 import re
 from random import shuffle
 random.seed(1)
 
-#stop words list
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 
-			'ours', 'ourselves', 'you', 'your', 'yours', 
-			'yourself', 'yourselves', 'he', 'him', 'his', 
-			'himself', 'she', 'her', 'hers', 'herself', 
-			'it', 'its', 'itself', 'they', 'them', 'their', 
-			'theirs', 'themselves', 'what', 'which', 'who', 
-			'whom', 'this', 'that', 'these', 'those', 'am', 
-			'is', 'are', 'was', 'were', 'be', 'been', 'being', 
-			'have', 'has', 'had', 'having', 'do', 'does', 'did',
-			'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or',
-			'because', 'as', 'until', 'while', 'of', 'at', 
-			'by', 'for', 'with', 'about', 'against', 'between',
-			'into', 'through', 'during', 'before', 'after', 
-			'above', 'below', 'to', 'from', 'up', 'down', 'in',
-			'out', 'on', 'off', 'over', 'under', 'again', 
-			'further', 'then', 'once', 'here', 'there', 'when', 
-			'where', 'why', 'how', 'all', 'any', 'both', 'each', 
-			'few', 'more', 'most', 'other', 'some', 'such', 'no', 
-			'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 
-			'very', 's', 't', 'can', 'will', 'just', 'don', 
-			'should', 'now', '']
+stop_words = stopwords.words("english")
 
 #cleaning up text
 def get_only_chars(line):
