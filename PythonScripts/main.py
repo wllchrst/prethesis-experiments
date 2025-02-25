a76cae0950bdfd88a4c5d70709134b4f1da851f5
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-25 19:14:20 +0700

    feat; use new settings

--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -4,29 +4,32 @@ from custom_dataset import CustomDataset, DatasetSettings
 from training import train_model, TrainingInformation
 
 if __name__ == "__main__":
+
     for dataset_info in DATASETS:
-        
+
         loader_settings = DataLoaderSettings(
                 dataset_link=dataset_info.link,
                 keys=['train', 'test', 'validation'],
                 label_col='label',
                 text_col='text',
+                columns_to_drop=[],
+                from_cache=False,
                 with_augmentation=dataset_info.augment,
                 hf_dataset=dataset_info.hf_dataset,
                 with_balancing=dataset_info.with_balancing
             )
-        
+
         data_loader = DataLoader(loader_settings)
 
         for model in MODELS:
             dataset_settings = DatasetSettings(
-                label_col='label', 
+                label_col='label',
                 text_col='text',
                 tokenizer_link=model
             )
 
             dataset = CustomDataset(dataset_settings, data_loader)
-            
+
             training_information = TrainingInformation(
                 dataset_name=dataset_info.link,
                 epoch=4,
