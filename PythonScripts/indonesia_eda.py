da04f90b12cdbd762582712be58662ca90858a8e
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-26 10:12:40 +0700

    feat: indonesia eda
    Using synonym replacement, back translation

---
+++ PythonScripts/indonesia_eda.py
@@ -0,0 +1,149 @@
+import random
+import re
+import asyncio
+from googletrans import Translator
+
+
+#cleaning up text
+def get_only_chars(line: str) -> str:
+    """Clean the sentence from each line
+
+    Args:
+        line (str): string that is going to be processed
+
+    Returns:
+        str: return the processed string
+    """
+
+    clean_line = ""
+
+    line = line.replace("’", "")
+    line = line.replace("'", "")
+    line = line.replace("-", " ") #replace hyphens with spaces
+    line = line.replace("\t", " ")
+    line = line.replace("\n", " ")
+    line = line.lower()
+
+    for char in line:
+        if char in 'qwertyuiopasdfghjklzxcvbnm ':
+            clean_line += char
+        else:
+            clean_line += ' '
+
+    clean_line = re.sub(' +',' ',clean_line) #delete extra spaces
+    if clean_line[0] == ' ':
+        clean_line = clean_line[1:]
+    return clean_line
+
+def load_synonyms(file_path):
+    """Load synonyms from a .txt file into a dictionary."""
+    synonym_dict = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            words = [word.strip() for word in line.split(",")]
+            for word in words:
+                synonym_dict[word] = [w for w in words if w != word]  # Exclude itself
+    return synonym_dict
+
+def load_stopwords(stopwords_path):
+    """Load stopwords from a file into a set."""
+    with open(stopwords_path, 'r', encoding='utf-8') as file:
+        return set(word.strip() for word in file)
+
+def get_synonyms(word: str) -> list[str]:
+    """Get a list synonym from a word 
+
+    Args:
+        word (str): word that will be target to finding the synonyms
+
+    Returns:
+        list[str]: return list of the word synonym
+    """
+    if word in synonyms_dict:
+        return synonyms_dict[word]
+    return []
+
+stop_words = load_stopwords("../ImportantFiles/id.stopwords.02.01.2016.txt")
+synonyms_dict = load_synonyms("../ImportantFiles/Tesaurus-BahasaIndonesia.txt")
+translator = Translator()
+
+def synonym_replacement(words: str, n: int) -> list[str]:
+    """Synonym replacement for text augmenation
+
+    Args:
+        words (str): list of words that is going to be replaced by the synonym
+        n (int): amount of changes
+
+    Returns:
+        list[str]: list of words has been replace with synonym
+    """
+
+    new_words = words.copy()
+    random_word_list = list(set([word for word in words if word not in stop_words]))
+    random.shuffle(random_word_list)
+    num_replaced = 0
+    for random_word in random_word_list:
+        synonyms = get_synonyms(random_word)
+        if len(synonyms) >= 1:
+            synonym = random.choice(list(synonyms))
+            new_words = [synonym if word == random_word else word for word in new_words]
+            #print("replaced", random_word, "with", synonym)
+            num_replaced += 1
+        if num_replaced >= n: #only replace up to n words
+            break
+
+    #this is stupid but we need it, trust me
+    sentence = ' '.join(new_words)
+    new_words = sentence.split(' ')
+
+    return new_words
+
+async def back_translation(sentence: str, src_lang='id', dest_lang='en') -> str:
+    """Back Transalation
+
+    Args:
+        sentence (str): Sentence that is going to be augmented
+        src_lang (str, optional): Orginal Language of the sentence. Defaults to 'id'.
+        dest_lang (str, optional): Target Language. Defaults to 'en'.
+
+    Returns:
+        str: strin that been augmented
+    """
+    english_version = await translator.translate(sentence, src=src_lang, dest=dest_lang)
+    back_translated = await translator.translate(english_version.text, src=dest_lang, dest=src_lang)
+
+    return back_translated.text
+
+async def indonesia_eda(sentence: str) -> list[str]:
+    """Fucntion to augment indonesian sentence, become 3 new sentence,
+    method that is used are Synonim Replacement and Back Translation  
+
+    Args:
+        sentence (str): Indonesia sentence that is going to be augmentated
+
+    Returns:
+        list[str]: Augmented Results
+    """
+    sentence = get_only_chars(sentence)
+    words = sentence.split(' ')
+    words = [word for word in words if word != '']
+
+    augmented_sentences = []
+
+    a_words = synonym_replacement(words, 2)
+    bt_argentina = await back_translation(sentence, dest_lang="ar")
+    bt_english = await back_translation(sentence)
+
+    augmented_sentences.append(" ".join(a_words))
+    augmented_sentences.append(bt_argentina)
+    augmented_sentences.append(bt_english)
+
+    return list(set(augmented_sentences))
+
+x = "Saya ingin bermain dengan teman di taman."
+print(x)
+
+sentences = asyncio.run(indonesia_eda(x))
+
+for sen in sentences:
+    print(sen)
