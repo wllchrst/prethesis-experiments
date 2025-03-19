a18467f35cf3c7560395bee116930a6fe57cb13f
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-03-19 07:39:11 +0700

    feat: bagging not tested

--- PythonScripts/ensemble/bagging.py
+++ PythonScripts/ensemble/bagging.py
@@ -3,6 +3,8 @@ from transformers import AutoModelForSequenceClassification, TrainingArguments, 
 from PythonScripts.training.training import TrainingInformation, compute_metrics
 from sklearn.utils import resample
 from PythonScripts.dataset.custom_dataset import CustomDataset
+from datasets import Dataset
+
 
 class Bagging:
     def __init__(
@@ -13,7 +15,7 @@ class Bagging:
         training_information: TrainingInformation
     ):
         self.model_paths = model_paths
-        self.models 
+        self.models = []
         self.num_model = num_model
         self.training_information = training_information
         self.dataset = dataset
@@ -26,9 +28,9 @@ class Bagging:
             np.random.choice(dataset_size, size=dataset_size, replace=True).tolist()
             for _ in range(self.num_model)
         ]
-        
+
         bootstrap_samples = [train_dataset.select(indices) for indices in bootstrap_indices]
-        
+
         training_args = TrainingArguments(
             output_dir="./results",
             num_train_epochs=self.training_information.epoch,
@@ -46,14 +48,59 @@ class Bagging:
             gradient_accumulation_steps=2,
             max_grad_norm=1
         )
-        
+
         callbacks = []
         if self.training_information.early_stopping_patience != 0:
-            callbacks = 
+            callbacks = EarlyStoppingCallback(self.training_information.early_stopping_patience)
+
         for i in range(self.num_model):
             sample = bootstrap_samples[i]
             model = AutoModelForSequenceClassification.from_pretrained(self.model_paths[i])
+
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=sample,
+                eval_dataset=self.dataset.val,
+                compute_metrics=compute_metrics,
+                callbacks=callbacks
+            )
             
-            trainer = Trainer()
+            trainer.train()
             
-            
+            self.models.append(model)
+
+
+    def predict(self, aggregation="majority"):
+        """
+        Perform inference using ensemble models.
+
+        Args:
+        - test_dataset: Dataset to predict.
+        - aggregation: "majority" for majority voting, "average" for probability averaging.
+
+        Returns:
+        - List of final predictions.
+        """
+        test_dataset = self.dataset.test
+
+        if not self.models:
+            raise ValueError("Models are not trained yet!")
+
+        all_predictions = []
+        
+        for model in self.models:
+            trainer = Trainer(model=model)
+            predictions = trainer.predict(test_dataset).predictions
+            all_predictions.append(predictions)
+
+        all_predictions = np.array(all_predictions)  # Shape: (num_models, num_samples, num_classes)
+
+        if aggregation == "majority":
+            final_predictions = np.argmax(np.sum(all_predictions, axis=0), axis=1)
+        elif aggregation == "average":
+            final_predictions = np.argmax(np.mean(all_predictions, axis=0), axis=1)
+        else:
+            raise ValueError("Invalid aggregation method. Use 'majority' or 'average'.")
+
+        return final_predictions
